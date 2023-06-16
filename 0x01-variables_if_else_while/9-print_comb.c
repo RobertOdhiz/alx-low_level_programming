ec0706044fbc952640ae2092383d510a5432d07c
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-16 14:14:44 -0700

    Final change

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,11 @@ int main(void)
 	int n = 0;
 	int j = 0;
 	
-	for ( n = 0; n < 10; n++)
+	for (n = 0; n < 10; n++)
 	{
-		for (j = 0; j < 10; j++)
-		{
-			putchar('0' + n);
-			putchar('0' + j);
-			putchar(',' + ' ');
-		}
+		putchar('0' + n);
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
