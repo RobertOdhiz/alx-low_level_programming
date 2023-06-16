2ba40c4a6360303baa4836e01f16ebfdda2c27d0
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-16 13:46:57 -0700

    Reversed

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lowercase = 'z';
+
+	while (lowercase >= 'a')
+		lowercase--;
+	putchar('\n');
+	return (0);
+}
