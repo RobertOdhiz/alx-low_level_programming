5298c735106b325c3a59a71e6ceacc1e68dfd4e0
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-16 13:55:37 -0700

    Printing base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+	char x = 'a';
+
+	for (n = 0; n < 10; n++)
+		putchar('0' + n);
+	while (x <= f)
+	{
+		putchar(x);
+		putchar++;
+	}
+	putchar('\n');
+	return (0);
+}
