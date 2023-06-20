9f7093f65154366c90fb3f228f74680dee2a134b
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-20 16:03:06 -0700

    New file caanges

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _abs - Computes absolute value of an iteger
+* @n:number being checked
+* Return: result or 0
+*/
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		int result;
+
+		result = n * -1;
+		return (result);
+	}
+	else
+		return (0);
+}
