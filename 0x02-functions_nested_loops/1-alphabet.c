390ee80d919e6528a27cfb18dd189dd66c653423
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-20 12:58:55 -0700

    New file created

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-void print_alphabet(void)
-{
-	char alpha = 'a';
-
-	while (alpha <= 'z')
-	{
-		putchar(alpha);
-		alpha++;
-	}
-	putchar('\n');
-}
 /**
  * main - check the code
  *
@@ -20,4 +9,16 @@ int main(void)
 {
 	print_alphabet();
 	return (0);
+}
+
+void print_alphabet(void)
+{
+        char alpha = 'a';
+
+        while (alpha <= 'z')
+        {
+                putchar(alpha);
+                alpha++;
+        }
+        putchar('\n');
 }
