ac2288bb36bf7e0f018ced4c5a4770791732ec3c
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-20 16:47:45 -0700

    New file caanges

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - returns sum f two integers
+* @a: first number
+* @b: second number to be addded
+* Return: sum of the two numbers
+*/
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
