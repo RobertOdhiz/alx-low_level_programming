85bdf6d71a846bb5a8c542fb1db74ace11fe37b2
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-20 17:10:59 -0700

    New file caanges

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			_putchar(n + '0');
+			_putchar(n);
 			if (n == 98)
 				break;
 			_putchar(',');
@@ -22,7 +22,7 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			_putchar(n + '0');
+			_putchar(n);
 			if (n == 98)
 				break;
 			_putchar(',');
