70c5d43a8355076fbd70589e00d8101fecb1eb29
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-21 09:12:41 -0700

    New file caanges

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints timetable for n
+ * @n: value being checked
+ * Return: ...
+ */
+void print_times_table(int n)
+{
+	int x, y, z, u, d;
+
+	for (x = 0; x < n; x++)
+	{
+		for (y = 0; y < n; y++)
+		{
+			z = x * y;
+			if (z > n)
+			{
+				d = z % 10;
+				u = (z - d) / 10;
+				_putchar(44);
+				_putchar(32);
+				_putchar(u + '0');
+				_putchar(d + '0');
+			}
+			else
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+			}
+			_putchar('\n');
+		}
+	}
+}
