ed73ac7db3335553ec58016b36a2c09926c06eab
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-22 15:58:36 -0700

    New file added

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,6 +9,10 @@ void print_triangle(int size)
 {
 	int x, y;
 
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
 	for (x = 0; x < size; x++)
 	{
 		for (y = i; y < size; y++)
@@ -16,5 +20,6 @@ void print_triangle(int size)
 		for (y = 1; y <= i; y++)
 			_putchar('#');
 		_putchar('\n');
+	}
 	}
 }
