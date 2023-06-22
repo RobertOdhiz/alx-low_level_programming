7db83bb7913a502e29ba91a55e270050c8185cff
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-22 13:58:48 -0700

    New file added

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal line
+ * @n: number of diagonals
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	while (n != 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
