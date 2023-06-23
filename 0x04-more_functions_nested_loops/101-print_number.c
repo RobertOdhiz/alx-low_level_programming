44c6a741fd114858e837043bb0bcdeade42df3f4
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-23 10:55:08 -0700

    New file added

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: number being checked
+ */
+
+void print_number(int n)
+{
+	int x, y, z, p;
+
+	if (n < 0)
+		_putchar('-');
+
+	if (n < 10)
+		_putchar(n + '0');
+	else if (n < 100 || n > (-100))
+	{
+		x = n % 10;
+		y = (n - x) / 10;
+		_putchar(y + '0');
+		_putchar(x + '0');
+	}
+	else if (n < 1000 || n > (-1000))
+	{
+		x = n % 10;
+		y = (n - x) % 100;
+		z = (n - ((10 * y) + x)) / 100;
+		_putchar(z + '0');
+		_putchar(y + '0');
+		_putchar(x + '0');
+	}
+	else if (n < 10000 || n > (-10000))
+	{
+		x = n % 10;
+		y = (n - x) % 100;
+		z = (n - ((10 * y) + x)) % 1000;
+		p = (n - ((100 * z) + (10 * y) + x)) / 1000;
+		_putchar(p + '0');
+		_putchar(z + '0');
+		_putchar(y + '0');
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
