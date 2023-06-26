a99f3678e84eb13182822b30317a64d78fba97e0
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-26 15:38:19 -0700

    Adding file for 0x05

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: character being checked
+ */
+
+void puts_half(char *str)
+{
+	int i, j = 0, n;
+
+	while (str[j] != '\0')
+		j++;
+	if (j % 2 == 0)
+		for (i = j / 2; i < j; i++)
+			_putchar(str[i]);
+	else
+	{
+		n = (j - 1) / 2;
+		for (i = n; i < j; i++)
+			_putchar(str[i]);
+	}
+}		
