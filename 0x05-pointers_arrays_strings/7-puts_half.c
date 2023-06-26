b2c33c8c6cdda82def959e74e973ec0fe74be2db
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-26 16:07:00 -0700

    Adding file for 0x05

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,14 +11,18 @@ void puts_half(char *str)
 
 	while (str[j] != '\0')
 		j++;
-	if (j % 2 == 0)
-		for (i = j / 2; i < j; i++)
-			_putchar(str[i]);
-	else
+	if (j % 2 == 1)
 	{
 		n = (j - 1) / 2;
 		n += 1;
 		for (i = n; i <= j; i++)
 			_putchar(str[i]);
 	}
+	else
+	{
+		for (i = (j / 2); i < j; i++)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+
 }
