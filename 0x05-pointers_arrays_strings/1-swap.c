057e7c5dfbd5ed4d9970c62d82d630f175ef7d30
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-26 14:24:30 -0700

    Adding file for 0x05

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap two integers
+ * @a: first value
+ * @b: second value
+ */
+
+void swap_int(int *a, int *b)
+{
+	int num;
+
+	*a = num;
+	*a = b;
+	*b = num;
+}
