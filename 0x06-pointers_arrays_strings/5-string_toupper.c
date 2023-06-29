921948df1f6915b96fc5d47dd7683fafe9481b11
Robert Odhiambo <odhizrober254@gmail.com>
2023-06-29 14:15:31 -0700

    New file for arrays, pointers and strings

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters to uppercase
+ * @arr: string being changed
+ * Return: Uppercase string
+ */
+
+char *string_toupper(char *arr)
+{
+	int i = 0;
+
+	while (arr[i] != '\0')
+	{
+		if (arr[i] >= 'a' && arr[i] <= 'z')
+			arr[i] -= 32;
+		i++;
+	}
+	return (0);
+}
