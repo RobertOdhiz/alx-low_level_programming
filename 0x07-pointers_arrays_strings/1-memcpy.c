1aebb039aee7d525f991e02dad293c14b19e0d13
Robert Odhiambo <odhizrober254@gmail.com>
2023-07-03 09:19:53 -0700

    New file for arrays, pointers and strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - Copies memory area
+ * @dest: Where memory is to be copied to
+ * @src: Where merory is copied from
+ * @n: Number of bytes
+ * REturn: Character
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		src[i] = dest[i];
+	return (dest);
+}
