2aa0d10872d343c6f69768d9be73af25765db9bd
Robert Odhiambo <odhizrober254@gmail.com>
2023-07-03 09:06:00 -0700

    New file for arrays, pointers and strings

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@ int _putchar(char c);
 
 char *_memset(char *s, char b, unsigned int n);
 
-char *memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 #include <stdio.h>
 
