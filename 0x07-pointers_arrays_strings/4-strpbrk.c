8cb8ff0632b6a737c91e7583ee4a6923981461fe
Robert Odhiambo <odhizrober254@gmail.com>
2023-07-03 10:48:39 -0700

    New file for arrays, pointers and strings

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Searches a string for any set of bytes
+ * @s: String to be scanned
+ * @accept: String containing characters to match
+ * Return: Character
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+			if (s[i] == accept[j])
+				return (s + i);
+	}
+	return (NULL);
+}
