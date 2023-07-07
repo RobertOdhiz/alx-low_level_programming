4463cb0fb45da67a19115bb285054a61855aeca4
Robert Odhiambo <odhizrober254@gmail.com>
2023-07-07 14:54:14 -0700

    Add file to 0x0A-argc_argv directory

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,16 +10,18 @@
 
 int main(int argc, char *argv[])
 {
-	int i, prod = 1;
+	int num1 = 0, num2 = 0;
 
-	if (argc > 1)
+	if (argc == 3)
 	{
-		for (i = 1; i < argc; i++)
-			prod = prod * atoi(argv[i]);
-		printf("%d\n", prod);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
 		return (0);
 	}
 	else
+	{
 		printf("Error\n");
-	return (1);
+		return (1);
+	}
 }
