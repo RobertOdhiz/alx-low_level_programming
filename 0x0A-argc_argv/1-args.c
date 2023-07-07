c63235e7b2d433874a924a936c6d77ff0ab609d3
Robert Odhiambo <odhizrober254@gmail.com>
2023-07-07 13:43:04 -0700

    Add file to 0x0A-argc_argv directory

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: count of arguments
+ * @argv - array of poignters to strings which are those arguments
+ * Return: always 0 for success
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
