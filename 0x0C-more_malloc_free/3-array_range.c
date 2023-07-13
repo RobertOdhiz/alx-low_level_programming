bfffec3616d766eb472a80dc5f2f89372a716daa
Joacim Odhiambo <joacim@sendyit.com>
2023-07-13 22:59:53 +0300

    new file added for 0x0c-more_malloc_free

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 	if (min > max)
 		return (NULL);
 
-	j = (sizeof(int) * (max - min)) + sizeof(int);;
+	j = (sizeof(int) * (max - min)) + sizeof(int);
 	ptr = malloc(j);
 
 	if (ptr == NULL)
