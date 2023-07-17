7a3b1922cf061f310d18c0407fd98a0701db1dd4
Joacim Odhiambo <joacim@sendyit.com>
2023-07-17 22:45:16 +0300

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defining a dog
+ * @name: member 1
+ * @age: member 2
+ * @owner: member 3
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
