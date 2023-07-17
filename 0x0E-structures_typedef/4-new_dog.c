b56290ba0bbc5b1237fe8962ea817ec9532034b4
Joacim Odhiambo <joacim@sendyit.com>
2023-07-18 00:48:42 +0300

    New dog-corrected

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -56,20 +56,19 @@ dog_t *new_dog(char *name, float age, char *owner)
 	ptr = malloc(sizeof(struct dog));
 	if (ptr == NULL)
 		return (NULL);
-	ptr->name = malloc(sizeof(char) * (i++));
+	ptr->name = malloc(sizeof(char) * (i + 1));
 	if (ptr->name == NULL)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	
-	ptr->owner = malloc(sizeof(char) * j++);
+	ptr->owner = malloc(sizeof(char) * (j + 1));
 	if (ptr->owner == NULL)
 	{
 		free(ptr);
+		free(ptr->name);
 		return (NULL);
 	}
-	
 	_strcpy(ptr->name, name);
 	ptr->age = age;
 	_strcpy(ptr->owner, owner);
