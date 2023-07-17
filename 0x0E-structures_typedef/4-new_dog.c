dfe1bc8b76880a41347c99650945ada7fa6d2a75
Joacim Odhiambo <joacim@sendyit.com>
2023-07-18 00:30:41 +0300

    New dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,42 @@
 #include "dog.h"
 
 /**
+ * _strlen - return length of the string
+ * @s: Sring being checked
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while(s[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * _strcpy - cpies a string pointed to by src
+ * @dest: destination
+ * @src: source
+ * Return: string copied(dest)
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len, int i;
+
+	len = 0;
+	while (src[len] != '\0')
+		len++;
+	for (i = 0; i < len; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
  * new_dog - Creates a new dog
  * @name: First par
  * @age: Second par
@@ -12,13 +48,30 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ptr;
+	int i, j;
 
+	i = _strlen(name);
+	j = _strlen(owner);
 	ptr = malloc(sizeof(struct dog));
 	if (ptr == NULL)
 		return (NULL);
-	ptr->name = name;
+	ptr->name = malloc(sizeof(char) * (i++));
+	if (ptr->name == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	
+	ptr->owner = malloc(sizeof(char) * j++);
+	if (ptr->owner == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	
+	_strcpy(dog->name, name);
 	ptr->age = age;
-	ptr->owner = owner;
+	_strcpy(ptr->owner, owner);
 
 	return (ptr);
 }
