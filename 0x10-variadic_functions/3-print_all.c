797e333923fb115be08c7965b775d86b9a38efa7
Robert Odhiambo <odhizrober254@gmail.com>
2023-07-20 13:13:09 -0700

    Printing everything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,55 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - Prints everything
- * @format: format of arguments
- * Return: Nothing
+ * print_all - prints everything
+ * @format: format argument
+ *
+ * Return: nothing
  */
 
 void print_all(const char * const format, ...)
 {
-	int count = 0, i = 0;
+	char choice, *str;
 	va_list list;
-	char choice;
+	int i = 0;
+	double num;
 
-	while (format[count])
-		count++;
+	va_start(list, format);
 
-	va_start(list, count);
-
-	while (i < count)
+	while (format && format[i])
 	{
-		i = 0;
-		format[i] = choice;
-		if (choice == 'i' ||
-			choice == 'c' ||
-			choice == 'f' ||
-			choice == 's')
-		{
-			switch (choice){
-				case 'c':
-					printf("%c", va_arg(list, char));
-					break;
-				case 'i':
-					printf("%d", va_arg(list, int));
-					break;
-				case 'f':
-					printf("%f", va_arg(list, float));
-					break;
-				case 's':
-					char *str = va_arg(list, char *);
+		choice = format[i];
 
-					if (str == NULL)
-					{
-						printf("(nil)");
-						break;
-					}
-					printf("%s", str);
-					break;
-				}
+		switch (choice)
+		{
+		case 'c':
+			printf("%c", va_arg(list, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(list, int));
+			break;
+		case 'f':
+			num = va_arg(list, double);
+			printf("%f", num);
+			break;
+		case 's':
+			str = va_arg(list, char *);
+
+			if (str == NULL)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", str);
+			break;
+		default:
+			i++;
+			continue;
 		}
+		if (format[i + 1] != '\0')
+			printf(", ");
 		i++;
 	}
+	va_end(list);
+	printf("\n");
 }
