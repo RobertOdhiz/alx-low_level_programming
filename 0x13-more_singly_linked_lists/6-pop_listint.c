b0d24558be913322afe04fdb2464a7ffa919e3d0
Robert Odhiambo <odhizrober254@gmail.com>
2023-07-31 14:45:10 -0700

    deleting head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes head node
+ * @head: pointer topointer to the head node
+ *
+ * Return: the head nodes data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr = *head;
+	int temp = ptr->n;
+
+	if (head == NULL || *head == NULL)
+		return (0);
+
+	*head = ptr->next;
+	ptr = *head;
+
+	return (temp);
+}
