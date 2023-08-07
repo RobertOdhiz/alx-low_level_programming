73b46aa54eea078d25aaba415808192f70982636
Robert Odhiambo <odhizrober254@gmail.com>
2023-08-07 09:55:58 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,17 +10,17 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
+	char *buf;
 	ssize_t fd, w, t;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buffer =  malloc(sizeof(char) * letters);
+	buf =  malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
 	w = write(STDOUT_FILENO, buf, t);
 
-	free(buffer);
+	free(buf);
 	close(fd);
 	return (w);
 }
