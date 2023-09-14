26dd3c309c5951d1b0869ea718bff512dc1749b0
Robert Odhiambo <odhizrober254@gmail.com>
2023-09-14 21:58:44 +0000

    changes on log

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,7 +8,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *ptr = h;
-	size_t count = 0;
+	size_t i = 0;
 
 	if (h == NULL)
 		return (0);
@@ -17,8 +17,8 @@ size_t print_dlistint(const dlistint_t *h)
 	{
 		printf("%d\n", ptr->n);
 		ptr = ptr->next;
-		count++;
+		i++;
 	}
 
-	return (count);
+	return (i);
 }
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,7 +8,7 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *ptr = h;
-	size_t count = 0;
+	size_t i = 0;
 
 	if (h == NULL)
 		return (0);
@@ -16,8 +16,8 @@ size_t dlistint_len(const dlistint_t *h)
 	while (ptr != NULL)
 	{
 		ptr = ptr->next;
-		count++;
+		i++;
 	}
 
-	return (count);
+	return (i);
 }
