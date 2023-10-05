fd9b2901ef8a39fda4c3203dadd989eb758ec9ee
Robert Odhiambo <odhizrober254@gmail.com>
2023-10-05 16:01:01 +0000

    Set value in a hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,78 @@
+#include "hash_tables.h"
+
+/**
+  * hash_table_set - Adds an elemnt to the hash table
+  * @ht: hash table being set
+  * @key: Key for the value
+  * @value: value set
+  * Return: 1 if it succeeded, 0 otherwise
+  */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+        unsigned long int index;
+        hash_node_t *new_node, *temp;
+        int check;
+
+        if (!ht || !key || key[0] == '\0' || value == NULL)
+                return (0);
+
+        index = key_index((const unsigned char *)key, ht->size);
+
+        temp = ht->array[index];
+
+        while (temp)
+        {
+                if (strcmp(temp->key, key) == 0)
+                {
+                        free(temp->value);
+                        temp->value = strdup(value);
+                        if (temp->value == NULL)
+                                return (0);
+                        return (1);
+                }
+                temp = temp->next;
+        }
+
+	new_node = malloc(sizeof(hash_node_t));
+        check = create_node(key, value, new_node);
+
+        if (check == 0)
+                return (0);
+
+        new_node->next = ht->array[index];
+        ht->array[index] = new_node;
+
+        return (1);
+}
+
+/**
+  * create_node - Creates a new node
+  * @key: key for the new node
+  * @value: value for new node
+  * @temp: pointer to the new node
+  * Return: 1 for success 0 otherwise
+  */
+
+int create_node(const char *key, const char *value, hash_node_t *temp)
+{
+        if (!temp)
+                return (0);
+
+        temp->key = strdup(key);
+        if (!temp->key)
+        {
+                free(temp);
+                return (0);
+        }
+
+        temp->value = strdup(value);
+        if (!temp->value)
+        {
+                free(temp->key);
+                free(temp);
+                return (0);
+        }
+
+        return (1);
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -53,6 +53,8 @@ char *hash_table_get(const hash_table_t *ht, const char *key);
 
 void hash_table_print(const hash_table_t *ht);
 
+int create_node(const char *key, const char *value, hash_node_t *temp);
+
 void hash_table_delete(hash_table_t *ht);
 
 #endif /* HASH_TABLES_H */
