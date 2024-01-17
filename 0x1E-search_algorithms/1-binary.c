0bdb394c1c6d779f487313d24e36e8b1e2e8bd8c
Robert Odhiambo <odhizrober254@gmail.com>
2024-01-17 22:01:46 +0300

    betty

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -16,7 +16,7 @@ int binary_search(int *array, size_t size, int value)
 
 	while (left <= right)
 	{
-		mid = left + (right - left)/2;
+		mid = left + (right - left) / 2;
 
 		printf("Searching in array: ");
 		for (i = left; i <= right; i++)
